b8382f6e86bb78e77fbb4facc79a649ee3721446
rbmateescu <rbmateescu@gmail.com>
2020-05-06 17:46:28 -0400

    removed rollout script

--- again.sh
+++
@@ -1,8 +0,0 @@
-oc project ham
-kubectl delete -f deploy/
-operator-sdk build $IMAGE
-docker push $IMAGE
-kubectl apply -f deploy
-oc project default
-sleep 20s
-kubectl logs `kubectl get pods -n ham | grep ham-application-assembler | head -n1 | awk '{print $1;}'` -n ham -f
